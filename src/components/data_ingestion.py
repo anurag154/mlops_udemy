b0c2d9eb7a425bafb6220410c2ce8cef282412cd
anurag154 <anuragsahu154@gmail.com>
2026-06-10 13:53:02 +0530

    data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,6 @@
+# python -m src.components.data_ingestion
+
+
 import os
 import sys
 from src.exception import CustomException
@@ -6,6 +9,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -47,6 +54,12 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+# on running this file artifacts folder will be created consisting of (data,test,train).csv
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
 
 
 
